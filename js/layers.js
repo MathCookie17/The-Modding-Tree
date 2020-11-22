517e5aa88b638bb51e53e7c1762314f5d7b36f19
MathCookie17 <74840506+MathCookie17@users.noreply.github.com>
2020-11-22 09:52:01 -0500

    v0.0.1, added pluses

--- js/layers.js
+++ js/layers.js
@@ -1,18 +1,37 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+// Calculate points/sec!
+function getPointGen() {
+	if(!canGenPoints())
+		return new Decimal(0)
+
+	let gain = new Decimal(1)
+	gain = gain.times(layers.plus.effect())
+	return gain
+}
+
+addLayer("plus", {
+    name: "pluses", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "+", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#0BE394",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    resource: "pluses", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
+    effect() {
+        let pointsoftcap = new Decimal(10)
+        let pluseffect = 0
+        if (player["plus"].points.lt(pointsoftcap)) {pluseffect = player["plus"].points.add(1)}
+        else {pluseffect = pointsoftcap.add(player["plus"].points.subtract(pointsoftcap).add(1).root(2))}
+        return pluseffect
+    },
+    effectDescription() {return "which multiply point gain by " + layers.plus.effect().times(100).round().div(100)
+    },
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
--- js/mod.js
+++ js/mod.js
@@ -1,20 +1,20 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "MathCookie17's First Testing Tree",
+	id: "MaCoTeTree001",
+	author: "MathCookie17",
 	pointsName: "points",
 	discordName: "",
 	discordLink: "",
 	changelogLink: "https://github.com/Acamaeda/The-Modding-Tree/blob/master/changelog.md",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	initialStartPoints: new Decimal (0), // Used for hard resets and new players
 	
 	offlineLimit: 1,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.0.1",
+	name: "Too small for a name",
 }
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
@@ -28,15 +28,6 @@ function getStartPoints(){
 // Determines if it should show points/sec
 function canGenPoints(){
 	return true
-}
-
-// Calculate points/sec!
-function getPointGen() {
-	if(!canGenPoints())
-		return new Decimal(0)
-
-	let gain = new Decimal(1)
-	return gain
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
