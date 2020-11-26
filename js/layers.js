66642be70fb55494143fc433089d49bd3dc50703
MathCookie17 <74840506+MathCookie17@users.noreply.github.com>
2020-11-26 15:50:19 -0500

    v0.0.3, added six plus upgrades
    Game is now developed up to 1e9 points.

--- js/layers.js
+++ js/layers.js
@@ -5,12 +5,16 @@ function getPointGen() {
 
 	let gain = new Decimal(1)
     gain = gain.times(layers.plus.effect())
+    if (hasUpgrade("plus", 13)) gain = gain.times(upgradeEffect("plus", 13))
+    if (hasUpgrade("plus", 33)) gain = gain.times(upgradeEffect("plus", 33))
 	return gain
 }
 
-function getPlusSoftcap() {
+function getPlusSoftcap(ust) {
 let plussoftcap = new Decimal(10)
-if (hasUpgrade("plus", 12)) plussoftcap = plussoftcap.mul(2)
+if (hasUpgrade("plus", 12)) plussoftcap = plussoftcap.mul(new Decimal(2).pow(ust))
+if (hasUpgrade("plus", 32)) plussoftcap = plussoftcap.mul(upgradeEffect("plus", 32).pow(ust))
+if (hasUpgrade("plus", 34)) plussoftcap = plussoftcap.mul(upgradeEffect("plus", 34).pow(ust))
 return plussoftcap
 }
 
@@ -45,7 +49,7 @@ addLayer("plus", {
     canReset() {return player.points.gte(9)},
     prestigeButtonText() {
         if (player.points.gte(9)) {
-            let plusButtonMessage = "Line up your points for <b>+" + layers.plus.getResetGain() + "</b> pluses"
+            let plusButtonMessage = "Line up your points for <b>+" + formatWhole(layers.plus.getResetGain()) + "</b> pluses"
             let nextAt = "<br>Next at <b>" + formatWhole(layers.plus.getNextAt()) + "</b> points"
             if (hasUpgrade("plus", 21)) plusButtonMessage = plusButtonMessage + nextAt
             return plusButtonMessage
@@ -54,25 +58,53 @@ addLayer("plus", {
     },
     effect() {
         let pluseffect = new Decimal(0)
-        if (player["plus"].points.lt(getPlusSoftcap())) {pluseffect = player["plus"].points.add(1)}
-        else {pluseffect = getPlusSoftcap().add(player["plus"].points.subtract(getPlusSoftcap()).add(1).root(2))}
+        if (player["plus"].points.lt(getPlusSoftcap(1))) {pluseffect = player["plus"].points.add(1)}
+        else {pluseffect = getPlusSoftcap(1).add(player["plus"].points.subtract(getPlusSoftcap(1)).add(1).root(2))}
         return pluseffect
     },
-    effectDescription() {return "which multiply point gain by " + format(layers.plus.effect().times(100).round().div(100)) + ". The current plus softcap is " + format(getPlusSoftcap()) + "."},
+    effectDescription() {return "which multiply point gain by " + format(layers.plus.effect().times(100).round().div(100)) + ". The current plus softcap is " + format(getPlusSoftcap(1)) + "."},
     row: 0, // Row the layer is in on the tree (0 is the first row)
     upgrades: {
         rows: 3,
-        cols: 2,
+        cols: 5,
         11: {
             title: "Plus One",
             description: "Gain one extra plus on reset",
             cost: new Decimal(18),
         },
         12: {
-            title: "Second Bottlecap",
+            title: "Slight Lifting",
             description: "Double the plus softcap",
             cost: new Decimal(90),
             unlocked() {return hasUpgrade("plus", 11)}
+        },
+        13: {
+            title: "Add Again",
+            description: "The plus multiplier is applied a second time, but without plus softcap increase upgrades",
+            cost: new Decimal(460),
+            effect() {
+                let capst = new Decimal(0)
+                if (hasUpgrade("plus", 14)) capst = new Decimal(0.5)
+                if (hasUpgrade("plus", 15)) capst = new Decimal(1)
+                let pluseffect = new Decimal(0)
+                if (player["plus"].points.lt(getPlusSoftcap(capst))) {pluseffect = player["plus"].points.add(1)}
+                else {pluseffect = getPlusSoftcap(capst).add(player["plus"].points.subtract(getPlusSoftcap(capst)).add(1).root(2))}
+                return pluseffect
+            },
+            effectDisplay() {return "x" + format(upgradeEffect("plus", 13))},
+            unlocked() {return hasUpgrade("plus", 12)},
+        },
+        14: {
+            title: "Secondary Lifting",
+            description: "Plus softcap increase upgrades have their square roots applied to Add Again",
+            cost: new Decimal(6260),
+            unlocked() {return hasUpgrade("plus", 13)}
+        },
+        15: {
+            title: "Two On The Ceiling",
+            description: "Plus softcap increase upgrades fully apply to Add Again",
+            cost: new Decimal(89400),
+            unlocked() {return hasUpgrade("plus", 14)}
         },
         21: {
             title: "Multiple Lineups",
@@ -86,7 +118,31 @@ addLayer("plus", {
             cost: new Decimal(58),
             effect() {return player.points.add(1).log(9).add(1)},
             effectDisplay() {return "x" + format(upgradeEffect("plus", 31))},
-            unlocked() {return hasUpgrade("plus", 21)}
+            unlocked() {return hasUpgrade("plus", 21)},
+        },
+        32: {
+            title: "Weight of the Points",
+            description: "The plus softcap is increased based on points",
+            cost: new Decimal(140),
+            effect() {return player.points.add(1).log(9).add(1)},
+            effectDisplay() {return "x" + format(upgradeEffect("plus", 32))},
+            unlocked() {return hasUpgrade("plus", 31)},
+        },
+        33: {
+            title: "Plus Decomposition",
+            description: "You gain more points based on pluses",
+            cost: new Decimal(9820),
+            effect() {return player["plus"].points.add(1).log(9).add(1)},
+            effectDisplay() {return "x" + format(upgradeEffect("plus", 33))},
+            unlocked() {return hasUpgrade("plus", 32)},
+        },
+        34: {
+            title: "Towering Pluses",
+            description: "The plus softcap is increased based on pluses",
+            cost: new Decimal(22222),
+            effect() {return player["plus"].points.add(1).log(9).add(1)},
+            effectDisplay() {return "x" + format(upgradeEffect("plus", 34))},
+            unlocked() {return hasUpgrade("plus", 33)},
         },
     },
     hotkeys: [
