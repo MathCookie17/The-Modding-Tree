f8dde80d4e87d0cafd2243bf5aaf262e1cfc94d7
MathCookie17 <74840506+MathCookie17@users.noreply.github.com>
2020-11-26 13:45:36 -0500

    Update layers.js
    v0.0.2, added first four plus upgrades

--- js/layers.js
+++ js/layers.js
@@ -4,8 +4,14 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	gain = gain.times(layers.plus.effect())
+    gain = gain.times(layers.plus.effect())
 	return gain
+}
+
+function getPlusSoftcap() {
+let plussoftcap = new Decimal(10)
+if (hasUpgrade("plus", 12)) plussoftcap = plussoftcap.mul(2)
+return plussoftcap
 }
 
 addLayer("plus", {
@@ -17,31 +23,74 @@ addLayer("plus", {
 		points: new Decimal(0),
     }},
     color: "#0BE394",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    requires: new Decimal(9), // Can be a function that takes requirement increases into account
     resource: "pluses", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    type: "custom",
+    getResetGain() {
+        let plusgain = new Decimal(0)
+        if (player.points.gte(9)) plusgain = plusgain.add(1)
+        if (hasUpgrade("plus", 21)) plusgain = player.points.div(9).root(2)
+        if (hasUpgrade("plus", 31)) plusgain = plusgain.times(upgradeEffect("plus", 31))
+        if (hasUpgrade("plus", 11)) plusgain = plusgain.add(1)
+        return plusgain.floor()
+    },
+    getNextAt() {
+        let nextAt = new Decimal(layers.plus.getResetGain().add(1))
+        if (hasUpgrade("plus", 11)) nextAt = nextAt.sub(1)
+        if (hasUpgrade("plus", 31)) nextAt = nextAt.div(upgradeEffect("plus", 31))
+        return nextAt.pow(2).mul(9)
+    },
+    canReset() {return player.points.gte(9)},
+    prestigeButtonText() {
+        if (player.points.gte(9)) {
+            let plusButtonMessage = "Line up your points for <b>+" + layers.plus.getResetGain() + "</b> pluses"
+            let nextAt = "<br>Next at <b>" + formatWhole(layers.plus.getNextAt()) + "</b> points"
+            if (hasUpgrade("plus", 21)) plusButtonMessage = plusButtonMessage + nextAt
+            return plusButtonMessage
+        }
+        else {return "You need at least <b>9</b> points to line them up!"}
+    },
     effect() {
-        let pointsoftcap = new Decimal(10)
-        let pluseffect = 0
-        if (player["plus"].points.lt(pointsoftcap)) {pluseffect = player["plus"].points.add(1)}
-        else {pluseffect = pointsoftcap.add(player["plus"].points.subtract(pointsoftcap).add(1).root(2))}
+        let pluseffect = new Decimal(0)
+        if (player["plus"].points.lt(getPlusSoftcap())) {pluseffect = player["plus"].points.add(1)}
+        else {pluseffect = getPlusSoftcap().add(player["plus"].points.subtract(getPlusSoftcap()).add(1).root(2))}
         return pluseffect
     },
-    effectDescription() {return "which multiply point gain by " + layers.plus.effect().times(100).round().div(100)
-    },
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
+    effectDescription() {return "which multiply point gain by " + format(layers.plus.effect().times(100).round().div(100)) + ". The current plus softcap is " + format(getPlusSoftcap()) + "."},
     row: 0, // Row the layer is in on the tree (0 is the first row)
+    upgrades: {
+        rows: 3,
+        cols: 2,
+        11: {
+            title: "Plus One",
+            description: "Gain one extra plus on reset",
+            cost: new Decimal(18),
+        },
+        12: {
+            title: "Second Bottlecap",
+            description: "Double the plus softcap",
+            cost: new Decimal(90),
+            unlocked() {return hasUpgrade("plus", 11)}
+        },
+        21: {
+            title: "Multiple Lineups",
+            description: "You can gain multiple pluses on reset",
+            cost: new Decimal(34),
+            unlocked() {return hasUpgrade("plus", 11)},
+        },
+        31: {
+            title: "Plus Overlap",
+            description: "You gain more pluses based on points",
+            cost: new Decimal(58),
+            effect() {return player.points.add(1).log(9).add(1)},
+            effectDisplay() {return "x" + format(upgradeEffect("plus", 31))},
+            unlocked() {return hasUpgrade("plus", 21)}
+        },
+    },
     hotkeys: [
-        {key: "p", description: "Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "p", description: "Reset for pluses", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
